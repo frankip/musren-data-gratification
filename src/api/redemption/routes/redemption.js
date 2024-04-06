777571f671e39fec1dd2769db3170ce831d0f34e
Frank <francis.kipchumba@moringaschool.com>
2024-04-06 04:08:08 +0300

    add custom route to be discoverable by swagger

--- src/api/redemption/routes/redemption.js
+++ src/api/redemption/routes/redemption.js
@@ -5,5 +5,27 @@
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
+const defaultRouter = createCoreRouter('api::redemption.redemption');
 
-module.exports = createCoreRouter('api::redemption.redemption');
+const customRouter = (innerRouter, extraRoutes = []) => {
+  let routes;
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes.concat(extraRoutes);
+      return routes;
+    },
+  };
+};
+
+const myExtraRoutes = [
+  {
+    method: "POST",
+    path: '/redemptions/gratify',
+    handler: "custom-controller.gratification",
+  },
+];
+
+module.exports = customRouter(defaultRouter, myExtraRoutes);
